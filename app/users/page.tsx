07c065147c88a1ba475df9a1c0d378876b7786ec
Elv Bert <elenavivianne.bertato@uzh.ch>
2026-03-26 15:34:52 +0100

    had to readd use client

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 // this code is part of S2 to display a list of all registered users
 // clicking on a user in this list will display /app/users/[id]/page.tsx
+"use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useApi } from "@/hooks/useApi";
