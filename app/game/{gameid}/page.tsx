e4fb4219c26ccc6208730391051cca73b0c86a26
Elv Bert <elenavivianne.bertato@uzh.ch>
2026-04-11 01:06:38 +0200

    #34 timer implementation

--- app/game/{gameid}/page.tsx
+++ app/game/{gameid}/page.tsx
@@ -6,7 +6,8 @@ import { Button, Input , message} from "antd";
 import styles from "@/styles/page.module.css";
 import { Game } from "@/types/game";
 import { Writer } from "@/types/writer";
-import React, { useEffect, useState } from "react";
+import { ApplicationError } from "@/types/error";
+import React, { useEffect, useState, useRef } from "react";
 import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
 
@@ -16,7 +17,7 @@ const GamePage: React.FC = () => {
   const params = useParams<{ gameid: string }>();
   const gameid = params?.gameid;
   const apiService = useApi();
-  
+  const [countdown, setCountdown] = useState<number>(0);
   const [game, setGame] = useState<Game | null>(null); 
   const [writer1Genre, setGenre1] = useState<string>("Genre"); 
   const [writer2Genre, setGenre2] = useState<string>("Genre");  
@@ -58,11 +59,19 @@ const {
 const [ wholeStoryText,setStoryyText] = useState<string>("");
 const [TwoInput, setTwoInput] = useState("");
 const [OneInput, setOneInput] = useState("");
-
+const autoSubmitted = useRef(false);
+const activeWriter = game?.writers.find((writer) => writer.turn);
+const CurrentUserisActiveWriter= activeWriter?.id===Number(userId);
+const isPlayer1Active = !!game?.writers[0]?.turn;
+const isPlayer2Active = !!game?.writers[1]?.turn;
+const isUserPlayer1 = game?.writers[0].id === Number(userId);
+const isUserPlayer2 = game?.writers[1].id === Number(userId);
 const handleSubmit = async (player: 1 | 2, input: string): Promise<void> => {
-  if (!input.trim()) return;
+  const prettyinput=input.trim();
   try {
-    const response=await apiService.post<Game>(`/games/${gameid}/input`,{ player: player, input: input },token);
+    const response=await apiService.post<Game>(`/games/${gameid}/input`,{ player: player, input: prettyinput },token);
+    setGame(response);
+    setCountdown(response.timer);
     const holeStoryText=response.story.text;
     setStoryyText(holeStoryText);
     if (player===2){
@@ -71,12 +80,25 @@ const handleSubmit = async (player: 1 | 2, input: string): Promise<void> => {
     else{
       setOneInput("")
     }
-  } catch (error) {
-    alert(`Saving player input failed, pls try again`);
-    console.log("Saving Player input failed", error);
-  }
+  } catch (error: unknown) {
+      if (error instanceof Error) {
+        const appError = error as ApplicationError; 
+        if(appError.status===403){
+           alert("It's not your turn");
+        }else {
+          alert(`Saving player input failed, pls try again`);
+          console.log("Saving Player input failed", error);
+        }
+      
+      }
 };
-
+}
+useEffect(() => {
+  if (!game) return;
+  if (countdown > 0) {
+    autoSubmitted.current = false;
+  }
+}, [countdown, game]);
 const handleExit=async() : Promise<void> =>{
   try{
     await apiService.post(`/games/${gameid}/leave`, {}, token);
@@ -94,6 +116,7 @@ useEffect(() => {
     try { 
       const ourGame: Game = await apiService.get<Game>(`/games/${gameid}`, token); 
       setGame(ourGame);
+      setCountdown(ourGame.timer);
       const writer1 = ourGame.writers[0];
       const writer2 = ourGame.writers[1];
 
@@ -101,6 +124,7 @@ useEffect(() => {
       setGenre2(writer2?.genre ?? "Genre");
 
       setStoryyText(ourGame.story.text);
+      
       
     } catch (error: unknown) {
       if (error instanceof Error) {
@@ -124,7 +148,10 @@ useEffect(() => {
 
     const checkIfGameStillExists = async () => {
     try {
-        await apiService.get(`/games/${gameid}`, token);
+        const latestGame=await apiService.get<Game>(`/games/${gameid}`, token);
+        setGame(latestGame);
+        setCountdown(latestGame.timer);
+        setStoryyText(latestGame.story.text);
       } catch (error: any) {
         if (error?.status === 404) {
           setGameEnded(true);
@@ -136,12 +163,12 @@ useEffect(() => {
     const id = setInterval(checkIfGameStillExists, 3000); //diese funktion wird unabhängig vom effekt vom browser alle 3 sekunden ausgeführt
 
     return () => clearInterval(id); //cleanup funktion, wenn die komponente verlassen wird oder der effekt neu läuft, stoppt die ständige funktionsausführung
-  }, [apiService, token, gameid, router, game]);
+  }, [apiService, token, gameid]);
 
   useEffect(() => { //executes the "cleanup", after a writer leave was detected
   if (!gameEnded) return;
 
-  message.error("Game ended because a writer left or disconnected.");
+  message.error("Game ended because a writer or judge left or disconnected.");
 
   const timeout = setTimeout(() => {
     router.push("/home");
@@ -149,6 +176,32 @@ useEffect(() => {
 
   return () => clearTimeout(timeout); //in case the component is unmounted (if the user redirects himself or something), before the timer expires, because then we obvioulsy don't want the message anymore
 }, [gameEnded, router]);
+
+useEffect(() => {
+  if (!game) return;
+  if (countdown <= 0) return;
+
+  const id = setTimeout(() => {
+    setCountdown((x) => x - 1);
+  }, 1000);
+
+  return () => clearTimeout(id);
+}, [countdown, game]);
+
+
+
+useEffect(() => {
+  if (!game) return;
+  if (countdown !== 0) return;
+  if (autoSubmitted.current) return;
+  if (!CurrentUserisActiveWriter) return;
+
+  const activePlayer = game.writers[0].turn ? 1 : 2;
+  const activeInput = activePlayer === 1 ? OneInput : TwoInput;
+
+  autoSubmitted.current = true;
+  handleSubmit(activePlayer, activeInput);
+}, [countdown, game, CurrentUserisActiveWriter, OneInput, TwoInput]);
 
 if (!game) { //beim ersten rendern ist user noch null, dann zeigen wir erst mal "loading"
   return <div>Loading Game...</div>;
@@ -327,6 +380,7 @@ return (
               }}
             >
               <TextArea
+                    disabled={!isUserPlayer1 || !game.writers[0].turn}
                     style={inputInnerStyle}
                     value={OneInput} //react kontrolliert das input feld, React setzt bei jedem Render den Wert des Input-Felds auf den aktuellen State wholestoryText. 
                     onChange={(e) => setOneInput(e.target.value)} //e ist das event objekt, e.target das input feld und e.target.value das was im feld steht
@@ -343,17 +397,18 @@ return (
               }}
             >
               <Input
-                value={game.writers[0].id === Number(userId) ? writer1Genre : "Genre"}
+                value={isUserPlayer1 ? writer1Genre : "Genre"}
                 readOnly
                 style={smallFieldStyle}
               />
               <Input
-                value="Timer"
+                value={isPlayer1Active ? countdown : "Timer" }
                 readOnly
                 style={{ ...smallFieldStyle, textAlign: "center" }}
               />
               <Button
               onClick={() =>handleSubmit(1,OneInput)}
+              disabled={!isUserPlayer1 || !game.writers[0].turn}
                 style={{
                   ["--btn-bg" as string]: "#2e9f44",
                   height: 40,
@@ -516,6 +571,7 @@ return (
               }}
             >
               <TextArea //controlled input
+                    disabled={!isUserPlayer2 || !game.writers[1].turn}
                     style={inputInnerStyle}
                     value={TwoInput} //react kontrolliert das input feld, React setzt bei jedem Render den Wert des Input-Felds auf den aktuellen State wholestoryText. 
                     onChange={(e) => setTwoInput(e.target.value)} //e ist das event objekt, e.target das input feld und e.target.value das was im feld steht
@@ -532,17 +588,18 @@ return (
               }}
             >
               <Input
-                value={game.writers[1].id === Number(userId) ? writer2Genre : "Genre"}
+                value={isUserPlayer2 ? writer2Genre : "Genre"}
                 readOnly
                 style={smallFieldStyle}
               />
               <Input
-                value="Timer"
+                value={isPlayer2Active ? countdown : "Timer" }
                 readOnly
                 style={{ ...smallFieldStyle, textAlign: "center" }}
               />
               <Button
               onClick={() =>handleSubmit(2,TwoInput)}
+              disabled={!isUserPlayer2 || !game.writers[1].turn}
                 style={{
                   ["--btn-bg" as string]: "#2e9f44",
                   height: 40,
