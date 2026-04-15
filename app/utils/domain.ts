eccc1bde5040c89b7f818c927d869a715cd60d91
Anto Grgic <anto.grgic@uzh.chm>
2026-04-15 09:21:29 +0200

    Fix domain

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -5,13 +5,9 @@ import { isProduction } from "@/utils/environment";
  * In production it retrieves the URL from NEXT_PUBLIC_PROD_API_URL (or falls back to a hardcoded url).
  * In development, it returns "http://localhost:8080".
  */
-/*export function getApiDomain(): string {
+export function getApiDomain(): string {
   const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
     "https://sopra-fs26-group14-server.oa.r.appspot.com"; // TODO: update with your production URL as needed.
   const devUrl = "http://localhost:8080";
   return isProduction() ? prodUrl : devUrl;
-}*/
-export function getApiDomain(): string {
-  const devUrl = "http://localhost:8080";
-  return devUrl;
 }
